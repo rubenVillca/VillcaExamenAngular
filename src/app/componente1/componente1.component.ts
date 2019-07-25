be2b76040120d4440993deb0da7b872a471acff9
Ruben Villca <r.villca.fernandez@gmail.com>
2019-07-24 21:35:31 -0400

    Pregunta4

--- src/app/componente1/componente1.component.ts
+++ src/app/componente1/componente1.component.ts
@@ -8,12 +8,18 @@ import {GeneradorService} from '../service/generador.service';
 })
 export class Componente1Component implements OnInit {
   private mensajeAleatorio: string;
+  private isDisplay: boolean;
+  private textoboton2: string;
 
   constructor(private generadorService: GeneradorService) {
+    this.isDisplay = true;
+    this.textoboton2 = 'Ocultar';
   }
 
   ngOnInit() {
     this.getWordGenerated();
+    this.isDisplay = true;
+    this.textoboton2 = 'Ocultar';
   }
 
   private getWordGenerated() {
@@ -21,5 +27,14 @@ export class Componente1Component implements OnInit {
       this.mensajeAleatorio = data.quote;
       return data;
     });
+  }
+
+  private setDisplayMesage() {
+    if (!this.isDisplay) {
+      this.textoboton2 = 'Ocultar';
+    } else {
+      this.textoboton2 = 'Mostrar';
+    }
+    return this.isDisplay = !this.isDisplay;
   }
 }
