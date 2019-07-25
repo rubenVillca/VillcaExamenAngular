7f60d516653414520a0bbc433f85409a1814bf8a
Ruben Villca <r.villca.fernandez@gmail.com>
2019-07-24 21:41:42 -0400

    pregunta5

--- src/app/componente1/componente1.component.ts
+++ src/app/componente1/componente1.component.ts
@@ -9,17 +9,21 @@ import {GeneradorService} from '../service/generador.service';
 export class Componente1Component implements OnInit {
   private mensajeAleatorio: string;
   private isDisplay: boolean;
+  private isDisplayButton1: boolean;
   private textoboton2: string;
+  private textoboton3: string;
 
   constructor(private generadorService: GeneradorService) {
     this.isDisplay = true;
-    this.textoboton2 = 'Ocultar';
+    this.isDisplayButton1 = true;
+    this.textoboton2 = 'Ocultar Mensaje';
+    this.textoboton3 = 'Ocultar boton generador';
   }
 
   ngOnInit() {
     this.getWordGenerated();
     this.isDisplay = true;
-    this.textoboton2 = 'Ocultar';
+    this.textoboton2 = 'Ocultar Mensaje';
   }
 
   private getWordGenerated() {
@@ -31,10 +35,19 @@ export class Componente1Component implements OnInit {
 
   private setDisplayMesage() {
     if (!this.isDisplay) {
-      this.textoboton2 = 'Ocultar';
+      this.textoboton2 = 'Ocultar mensaje';
     } else {
-      this.textoboton2 = 'Mostrar';
+      this.textoboton2 = 'Mostrar mensaje';
     }
     return this.isDisplay = !this.isDisplay;
+  }
+
+  private displayButton1() {
+    if (!this.isDisplayButton1) {
+      this.textoboton3 = 'Ocultar boton generador';
+    } else {
+      this.textoboton3 = 'Mostrar boton generador';
+    }
+    return this.isDisplayButton1 = !this.isDisplayButton1;
   }
 }
